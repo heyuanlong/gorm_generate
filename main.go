5476127015d3f3858d107b705b6bbe13efd090b8
heyuanlong <1023417614@qq.com>
2018-12-03 11:03:28 +0800

    sfdsdf

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bytes"
+	ksql "database/sql"
 	"flag"
+	"fmt"
 	"strings"
 
 	kconf "github.com/heyuanlong/go-utils/common/conf"
@@ -16,54 +19,88 @@ func init() {
 
 //gorm_generate.exe -s beegoblog -t users
 
-var sql string
-
 func main() {
 	table_schema := flag.String("s", "test", "table_schema")
 	table_name := flag.String("t", "test", "table_name")
 	flag.Parse()
 
-	header(*table_name)
-
-	sqlStr := "select column_name,data_type from information_schema.columns where table_schema=? and table_name=?;"
+	sqlStr := "select column_name,data_type,column_key from information_schema.columns where table_schema=? and table_name=?;"
 	rows, err := kmysql.MysqlClient.Query(sqlStr, *table_schema, *table_name)
 	if err != nil {
 		klog.Error.Println("get  fail :", err)
 		return
 	}
+	createStr := createTable(*table_name, rows)
+	rows, err = kmysql.MysqlClient.Query(sqlStr, *table_schema, *table_name)
+	insertStr := insertTable(*table_name, rows)
+
+	klog.Info.Println("\n", createStr, "\n", insertStr)
+}
+
+//---------------------------------------------------------------------------
+func createTable(table_name string, rows *ksql.Rows) string {
+	var sql string
+	sql += "type " + converUpper(table_name) + " struct {\n"
 	for rows.Next() {
 		column_name := ""
 		data_type := ""
-		err = rows.Scan(&column_name, &data_type)
+		column_key := ""
+		err := rows.Scan(&column_name, &data_type, &column_key)
 		if err != nil {
 			klog.Error.Println("scan fail :", err)
-			return
+			return ""
 		}
-		//klog.Info.Println("column_name:", converUpper(column_name), " data_type:", data_type)
-		row(column_name, data_type)
+		if "ID" == strings.ToUpper(column_name) {
+			sql += "\tID     uint64  `gorm:\"primary_key\" json:\"-\"`\n"
+		} else {
+			sql += "\t" + converUpper(column_name) + " " + getType(data_type, column_name) + " `gorm:\"column:" + column_name + "\" json:\"" + column_name + "\"`\n"
+		}
 	}
-	tail(*table_name)
-	klog.Info.Println("ok\n")
-	klog.Info.Println(sql)
-}
-
-func header(name string) {
-	sql += "type " + converUpper(name) + " struct {\n"
-}
-func row(name, types string) {
-	if "ID" == strings.ToUpper(name) {
-		sql += "\tID     uint64  `gorm:\"primary_key\" json:\"-\"`\n"
-	} else {
-		sql += "\t" + converUpper(name) + " " + getType(types) + "`gorm:\"column:" + name + "\" json:\"" + name + "\"`\n"
-	}
-}
-func tail(name string) {
 	sql += "}\n"
-	sql += "func (" + converUpper(name) + ") TableName() string {\n"
-	sql += "\treturn \"" + name + "\"\n}"
+	sql += "func (" + converUpper(table_name) + ") TableName() string {\n"
+	sql += "\treturn \"" + table_name + "\"\n}"
+
+	return sql
 }
 
-func getType(s string) string {
+//---------------------------------------------------------------------------
+func insertTable(table_name string, rows *ksql.Rows) string {
+	var bf bytes.Buffer
+	var bt bytes.Buffer
+	//func InsertCoinTransformLog(tx *jgorm.DB, user_id, dst_coin_id, src_coin_id int, dst_coins, src_coins, trans float64, order_id int) (kmodel.FmCoinTransformLog, error) {
+	fmt.Fprintf(&bf, "func Insert%s(tx *jgorm.DB", converUpper(table_name))
+	fmt.Fprintf(&bt, ` { 
+	if tx == nil {  
+		tx = gorm.GormDB 
+	}`)
+	fmt.Fprintf(&bt, "\n\tobj := kmodel.%s{\n", converUpper(table_name))
+	for rows.Next() {
+		column_name := ""
+		data_type := ""
+		column_key := ""
+		rows.Scan(&column_name, &data_type, &column_key)
+		fmt.Fprintf(&bf, ", %s %s", column_name, getType(data_type, column_name))
+		if column_key != "PRI" {
+			fmt.Fprintf(&bt, "\t\t%s:%s,\n", converUpper(column_name), column_name)
+		}
+	}
+	fmt.Fprintf(&bf, ")(kmodel.%s,error)", converUpper(table_name))
+	fmt.Fprintf(&bt, `	}
+	if err := tx.Create(&obj).Error; err != nil {
+		klog.Error.Println(err)
+		return obj, err
+	}
+	return obj, nil
+}`)
+
+	return bf.String() + bt.String()
+}
+
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+
+func getType(s, name string) string {
 	if s == "smallint" {
 		return "int"
 	}
@@ -77,6 +114,9 @@ func getType(s string) string {
 		return "int64"
 	}
 	if s == "int" {
+		if strings.Index(name, "time") != -1 {
+			return "int64"
+		}
 		return "int"
 	}
 	if s == "text" {
